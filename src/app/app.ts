f50b09d825eb3da10a386481d38066d93ef3ddf9
MuhammadJr <Muhammadou1drammeh@gmail.com>
2025-12-29 16:07:28 -0600

    initial commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,28 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { UserComponent } from './components/user/user';
+import { RandomUserService } from './services/random-user';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  standalone: true,
+  imports: [CommonModule, UserComponent],
+  templateUrl: './app.html'
 })
-export class App {
-  protected readonly title = signal('Radom-PersonProfile');
+export class App implements OnInit {
+
+  user: any = {};
+
+  constructor(private randomUserService: RandomUserService) {}
+
+  ngOnInit(): void {
+    this.loadUser();
+  }
+
+  loadUser(): void {
+    this.randomUserService.getRandomUser().subscribe(response => {
+      this.user = response.results[0];
+    });
+  }
 }
+
